4d2ca206ef1f837a3513a26367516700eda592eb
stormy <mkubesch@gmail.com>
2018-10-26 19:55:17 +0200

    Frontpage HTML now generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -13,13 +13,12 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
+        # Edith has heard about a cool new online to-do app. She goes to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text 
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
@@ -38,10 +37,11 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Bux peacock feathers' for row in rows)
+            any(row.text == '1: Bux peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
         # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # enters "Use peacock feathers to make a fly"(Edith is very methodical)
         self.fail('Finsh the test!')
 
         # The page updates again, and now shows both items on her list
